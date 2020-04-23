426dc6240b681b0be205949e4c77bb071da82c90
Billy Gingrich <whgingrich@gmail.com>
2020-04-23 14:08:04 -0400

    day 2 mvp

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -58,17 +58,48 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node.left is not None:
+            node.in_order_print(node.left)
+        
+        print(node.value)
+
+        if node.right is not None:
+            node.in_order_print(node.right)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        node_queue = Queue()
+        node_queue.enqueue(node)
+        
+        while node_queue.size > 0:
+            node = node_queue.dequeue()
+            print(node.value)
+            
+            if node.left is not None:
+                node_queue.enqueue(node.left)
+            
+            if node.right is not None:
+                node_queue.enqueue(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        node_stack = Stack()
+        node_stack.push(node)
+
+        while node_stack.size > 0:
+
+            node = node_stack.pop()
+            print(node.value)
+
+            if node.left is not None:
+                node_stack.push(node.left)
+            
+            if node.right is not None:
+                node_stack.push(node.right)
+
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
